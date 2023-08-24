2f90f16e4fa94e98e1728a09565c95cf3b052f7d
luancaius <luan.ca@outlook.com>
2023-08-23 18:11:13 -0600

    removing unused code

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -13,69 +13,25 @@ namespace Console
             
             bool running = true;
             DeputadoService deputadoService = new DeputadoService(jsonRepository, api1Service, api2Service);
+            
+            System.Console.Write("Enter a command: ");
+            string command = "b"; //Console.ReadLine().ToLower();
 
-            while (running)
+            System.Console.WriteLine($"Executing command {command}");
+            switch (command)
             {
-                System.Console.Write("Enter a command: ");
-                string command = "b"; //Console.ReadLine().ToLower();
-
-                System.Console.WriteLine($"Executing command {command}");
-                switch (command)
-                {
-                    case "a":
-                        await deputadoService.Api1_GetAllDeputados_SaveOnMongoDB();
-                        break;
-                    case "b":
-                        await deputadoService.Api2_GetAllDeputados_SaveOnMongoDB();
-                        break;
-                    default:
-                        System.Console.WriteLine("Invalid command. Please try again.");
-                        break;
-                }
+                case "a":
+                    await deputadoService.Api1_GetAllDeputados_SaveOnMongoDB();
+                    break;
+                case "b":
+                    await deputadoService.Api2_GetAllDeputados_SaveOnMongoDB();
+                    break;
+                default:
+                    System.Console.WriteLine("Invalid command. Please try again.");
+                    break;
             }
-
+            
             System.Console.WriteLine("Exiting the Command Console App. Goodbye!");
-        }
-
-
-        public static async Task Pokemon()
-        {
-            // Base URL of the API you want to call
-            string apiUrl = "https://pokeapi.co/api/v2/pokemon/ditto";
-
-            // Create an instance of HttpClient to send the HTTP request
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    // Send a GET request to the API and get the response
-                    HttpResponseMessage response = await client.GetAsync(apiUrl);
-
-                    // Check if the response is successful
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // Read the response content as a string
-                        string responseBody = await response.Content.ReadAsStringAsync();
-
-                        // Display the response content
-                        System.Console.WriteLine("API response:");
-                        System.Console.WriteLine(responseBody);
-                    }
-                    else
-                    {
-                        // Display an error message if the API call is not successful
-                        System.Console.WriteLine($"API call failed with status code: {response.StatusCode}");
-                    }
-                }
-                catch (HttpRequestException e)
-                {
-                    // Display an error message if there's an exception during the API call
-                    System.Console.WriteLine($"API call failed with exception: {e.Message}");
-                }
-            }
-
-            System.Console.WriteLine("Press any key to exit...");
-            System.Console.ReadKey();
         }
     }
 }
