fdee6870a803db2ee6de9289c63fdb89acaca3ce
luancaius <luan.ca@outlook.com>
2023-09-29 23:37:36 -0600

    adding dbsets

--- src/RelationalDatabase/Database/AppDbContext.cs
+++ src/RelationalDatabase/Database/AppDbContext.cs
@@ -5,9 +5,11 @@ namespace RelationalDatabase.Database;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Deputy> Deputies { get; set; }
+    
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-
-    public DbSet<Person> Persons { get; set; }
 }
