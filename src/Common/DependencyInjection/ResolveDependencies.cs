bbebc6ec0950ddb04f1eb2312d6c0d9da830af64
luancaius <luan.ca@outlook.com>
2023-10-08 21:32:33 -0600

    issues related to dependency injection fixed. ALmost making fetch call for all deputies

---
+++ src/Common/DependencyInjection/ResolveDependencies.cs
@@ -0,0 +1,67 @@
+using CacheDatabase.Interfaces;
+using CacheDatabase.Repositories;
+using ExternalAPI.Implementation;
+using ExternalAPI.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Repositories.Implementation;
+using Repositories.Interfaces;
+using Services.Interfaces;
+using Services.Service;
+using StackExchange.Redis;
+
+namespace DependencyInjection;
+
+public class ResolveDependencies
+{
+    private readonly ServiceProvider _serviceProvider;
+
+    public ResolveDependencies()
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+    }
+    
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        #region Infrastructure
+        
+        // MongoDB Configuration
+        // string mongoConnectionString = "mongodb://root:root@localhost:27017";
+        // string mongoDatabaseName = "congresso";
+        //
+        // string tableNameApi1 = "api1_deputados";
+        // string tableNameApi2 = "api2_deputados";
+        // string tableNameApi1Despesas = "api1_deputado_despesas";
+        // string tableNameApi2ListaPresenca = "api2_lista_presenca_deputados";
+        //     
+        // services.AddSingleton<MongoDbContext>(sp => new MongoDbContext(mongoConnectionString, mongoDatabaseName));
+        // services.AddTransient<Api1DeputadoMongoRepository>(sp => new Api1DeputadoMongoRepository(sp.GetRequiredService<MongoDbContext>(), tableNameApi1));
+        // services.AddTransient<Api2DeputadoMongoRepository>(sp => new Api2DeputadoMongoRepository(sp.GetRequiredService<MongoDbContext>(), tableNameApi2));
+        // services.AddTransient<Api1DeputadoDespesasMongoRepository>(sp => new Api1DeputadoDespesasMongoRepository(sp.GetRequiredService<MongoDbContext>(), tableNameApi1Despesas));
+        // services.AddTransient<Api2DeputadoListaPresencaMongoRepository>(sp => new Api2DeputadoListaPresencaMongoRepository(sp.GetRequiredService<MongoDbContext>(), tableNameApi2ListaPresenca));
+        
+        // Redis Configuration
+        string redisConnectionString = "localhost:6379";
+        services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnectionString));
+        services.AddTransient<IDatabase>(sp => sp.GetRequiredService<IConnectionMultiplexer>().GetDatabase());
+        services.AddTransient<IRedisCacheRepository, RedisCacheRepository>();
+
+        services.AddTransient<IBaseApi, BaseApi>();
+        services.AddTransient<IDadosAbertosOldApi, DadosAbertosOldApi>();
+        services.AddTransient<IDadosAbertosNewApi, DadosAbertosNewApi>();
+        
+        services.AddTransient<ISearchDeputyRepository, SearchDeputyRepository>();
+        services.AddTransient<IDeputyRepository, DeputyRepository>();
+        #endregion
+
+        #region Application
+        services.AddTransient<IDeputyService, DeputyService>();       
+        #endregion
+    }
+
+    public T Resolve<T>()
+    {
+        return _serviceProvider.GetService<T>() ?? throw new InvalidOperationException();
+    }
+}
--- src/Presentation/Presentation.Console/Program.cs
+++ src/Presentation/Presentation.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using DepencyInjection;
+﻿using DependencyInjection;
+using Services.Interfaces;
 
 namespace Presentation.Console
 {
@@ -6,11 +7,51 @@ namespace Presentation.Console
     {
         static async Task Main(string[] args)
         {
-            var resolver = new ResolveDependencies();
+            try 
+            {
+                var resolver = new ResolveDependencies();
             
-            var consoleApp = resolver.Resolve<ConsoleApp>();
+                var _deputyService = resolver.Resolve<IDeputyService>();
 
-            await consoleApp.ExecuteConsole();
+                await ExecuteConsole(_deputyService);            
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+
+        }
+        
+        public static async Task ExecuteConsole(IDeputyService _deputyService)
+        {
+            string command = "a";
+
+            System.Console.WriteLine($"Executing command {command}");
+            switch (command)
+            {
+                case "a":
+                    var deputies = await _deputyService.GetAllDeputyRaw(57);
+                    System.Console.WriteLine(deputies);
+                    break;
+                // case "b":
+                //     await deputadoService.Api2_GetAllDeputados_SaveOnMongoDB();
+                //     break;
+                // case "c":
+                //     await deputadoService.Api1_GetDeputadoDespesasByYear_SaveOnMongoDB(2023);
+                //     break;
+                // case "d":
+                //     await deputadoService.Api2_GetListaPresencaDeputado_SaveOnMongoDB(2023);
+                //     break;
+                // case "e":
+                //     await deputadoService.Api2_GetAllDeputados_SaveOnMongoDB();
+                //     await deputadoService.Api2_GetListaPresencaDeputado_SaveOnMongoDB(2023);
+                //     break;
+                default:
+                    System.Console.WriteLine("Invalid command. Please try again.");
+                    break;
+            }
+
+            System.Console.WriteLine("Exiting the Command Console App. Goodbye!");
         }
     }
 }
