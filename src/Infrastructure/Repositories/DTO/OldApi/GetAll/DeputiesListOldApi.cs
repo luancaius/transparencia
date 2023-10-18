2d527b5245ac6ba3709296b7c7c85b803201c96f
luancaius <luan.ca@outlook.com>
2023-10-18 15:54:34 -0600

    trying to parse xml inside the constructor

--- src/Infrastructure/Repositories/DTO/OldApi/GetAll/DeputiesListOldApi.cs
+++ src/Infrastructure/Repositories/DTO/OldApi/GetAll/DeputiesListOldApi.cs
@@ -2,24 +2,45 @@ using System.Xml.Serialization;
 
 namespace Repositories.DTO.OldApi.GetAll;
 
-[XmlRoot(ElementName = "deputados")]
 public class DeputiesListOldApi
 {
     public DeputiesListOldApi(String rawDeputiesList)
     {
         try
         {
-            var serializer = new XmlSerializer(typeof(DeputiesListOldApi));
+            var serializer = new XmlSerializer(typeof(DeputiesListOldApiSerializer));
             var reader = new StringReader(rawDeputiesList);
-            var deputies = (DeputiesListOldApi)serializer.Deserialize(reader);
-            Deputies = deputies.Deputies;   
+            var deputies = (DeputiesListOldApiSerializer)serializer.Deserialize(reader);
+            Console.WriteLine(deputies);
+            
         }
         catch(Exception e)
         {
             Console.WriteLine(e);
             throw;
         }
+        
+        // try
+        // {
+        //     XmlDocument doc = new XmlDocument();
+        //     doc.LoadXml(rawDeputiesList);
+        //     XmlNodeList nodes = doc.DocumentElement.SelectNodes("//deputados/deputado");
+        //     var serializer = new XmlSerializer(typeof(DeputiesListOldApiSerializer));
+        //     foreach (XmlNode node in nodes)
+        //     {
+        //         using (XmlNodeReader reader = new XmlNodeReader(node))
+        //         {
+        //             var deputado = (DeputyOldApi)serializer.Deserialize(reader);
+        //             Console.WriteLine(deputado);
+        //         }
+        //     }
+        // }
     }
 
+}
+
+[XmlRoot(ElementName = "deputados")]
+public class DeputiesListOldApiSerializer
+{
     public List<DeputyOldApi> Deputies { get; set; }
 }
