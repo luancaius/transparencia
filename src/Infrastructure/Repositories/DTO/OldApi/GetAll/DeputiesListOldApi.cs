68266772c36f42e1e3816e74a841a2dc9ae60c19
luancaius <luan.ca@outlook.com>
2023-10-19 22:52:03 -0600

    parsing deputies list old api

--- src/Infrastructure/Repositories/DTO/OldApi/GetAll/DeputiesListOldApi.cs
+++ src/Infrastructure/Repositories/DTO/OldApi/GetAll/DeputiesListOldApi.cs
@@ -1,3 +1,4 @@
+using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace Repositories.DTO.OldApi.GetAll;
@@ -8,39 +9,48 @@ public class DeputiesListOldApi
     {
         try
         {
-            var serializer = new XmlSerializer(typeof(DeputiesListOldApiSerializer));
-            var reader = new StringReader(rawDeputiesList);
-            var deputies = (DeputiesListOldApiSerializer)serializer.Deserialize(reader);
-            Console.WriteLine(deputies);
-            
+            XDocument doc = XDocument.Parse(rawDeputiesList);
+            XNamespace soap = "http://schemas.xmlsoap.org/soap/envelope/";
+            XNamespace camara = "https://www.camara.gov.br/SitCamaraWS/Deputados";
+            XNamespace empty = "";
+
+            Deputies = doc.Descendants(soap + "Body")
+                .Descendants(camara + "ObterDeputadosResponse")
+                .Descendants(camara + "ObterDeputadosResult")
+                .Descendants(empty + "deputados")
+                .Descendants("deputado")
+                .Select(d => new DeputyOldApi
+                {
+                    IdeCadastro = (int)d.Element("ideCadastro"),
+                    CodOrcamento = (string)d.Element("codOrcamento"),
+                    Condicao = (string)d.Element("condicao"),
+                    Matricula = (int)d.Element("matricula"),
+                    IdParlamentar = (int)d.Element("idParlamentar"),
+                    Nome = (string)d.Element("nome"),
+                    NomeParlamentar = (string)d.Element("nomeParlamentar"),
+                    UrlFoto = (string)d.Element("urlFoto"),
+                    Sexo = (string)d.Element("sexo"),
+                    Uf = (string)d.Element("uf"),
+                    Partido = (string)d.Element("partido"),
+                    Gabinete = (string)d.Element("gabinete"),
+                    Anexo = (string)d.Element("anexo"),
+                    Fone = (string)d.Element("fone"),
+                    Email = (string)d.Element("email"),
+                    Comissoes = new Comissoes
+                    {
+                        Titular = (string)d.Element("comissoes").Element("titular"),
+                        Suplente = (string)d.Element("comissoes").Element("suplente"),
+                    }
+                }).ToList();
+
+            Console.WriteLine(Deputies);
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             Console.WriteLine(e);
             throw;
         }
-        
-        // try
-        // {
-        //     XmlDocument doc = new XmlDocument();
-        //     doc.LoadXml(rawDeputiesList);
-        //     XmlNodeList nodes = doc.DocumentElement.SelectNodes("//deputados/deputado");
-        //     var serializer = new XmlSerializer(typeof(DeputiesListOldApiSerializer));
-        //     foreach (XmlNode node in nodes)
-        //     {
-        //         using (XmlNodeReader reader = new XmlNodeReader(node))
-        //         {
-        //             var deputado = (DeputyOldApi)serializer.Deserialize(reader);
-        //             Console.WriteLine(deputado);
-        //         }
-        //     }
-        // }
     }
 
-}
-
-[XmlRoot(ElementName = "deputados")]
-public class DeputiesListOldApiSerializer
-{
     public List<DeputyOldApi> Deputies { get; set; }
 }
