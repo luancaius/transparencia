4243b9285bb8b1165fa7bd060cfdd2ae062aba56
luancaius <luan.ca@outlook.com>
2023-12-21 23:29:04 -0700

    adding migrations for ids and cpf constraint

--- src/Infrastructure/RelationalDatabase/Migrations/20231222062826_ids_and_constraint.Designer.cs
+++ src/Infrastructure/RelationalDatabase/Migrations/20231222062826_ids_and_constraint.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RelationalDatabase.Database;
 
@@ -11,9 +12,11 @@ using RelationalDatabase.Database;
 namespace RelationalDatabase.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231222062826_ids_and_constraint")]
+    partial class ids_and_constraint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
