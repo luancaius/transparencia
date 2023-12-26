ddc11cedfca3b7c83b0f3aab07e4ac15d27ec8e4
luancaius <luan.ca@outlook.com>
2023-12-25 23:47:50 -0700

    adding migrations for new properties

---
+++ src/Infrastructure/RelationalDatabase/Migrations/20231226064604_removing_duplication.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RelationalDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class removing_duplication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                schema: "congresso",
+                table: "deputado");
+
+            migrationBuilder.DropColumn(
+                name: "SiglaUf",
+                schema: "congresso",
+                table: "deputado");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeEleitoral",
+                schema: "congresso",
+                table: "deputado",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeEleitoral",
+                schema: "congresso",
+                table: "deputado",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                schema: "congresso",
+                table: "deputado",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SiglaUf",
+                schema: "congresso",
+                table: "deputado",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
