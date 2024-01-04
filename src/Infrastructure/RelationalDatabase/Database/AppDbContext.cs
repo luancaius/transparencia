8e9c4c6e261498aa2720d01644e39729f384ad25
luancaius <luan.ca@outlook.com>
2024-01-03 22:45:31 -0700

    adding migration for supplier

--- src/Infrastructure/RelationalDatabase/Database/AppDbContext.cs
+++ src/Infrastructure/RelationalDatabase/Database/AppDbContext.cs
@@ -9,6 +9,7 @@ public class AppDbContext : DbContext
 {
     public DbSet<Deputado> Deputados { get; set; }
     public DbSet<Company> Empresas { get; set; }
+    public DbSet<Supplier> Fornecedores { get; set; }
     public DbSet<DeputyExpense> DeputadoDespesas  { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
