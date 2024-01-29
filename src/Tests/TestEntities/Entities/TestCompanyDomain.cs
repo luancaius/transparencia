997ad814524b3571b6ef8f8470a139eae035d5cc
luancaius <luan.ca@outlook.com>
2024-01-29 16:27:54 -0700

    adding test company domain

---
+++ src/Tests/TestEntities/Entities/TestCompanyDomain.cs
@@ -0,0 +1,26 @@
+using Entities.Entities;
+using Entities.ValueObject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestEntities.Entities;
+
+[TestClass]
+public class TestCompanyDomain
+{
+    [TestMethod]
+    public void CreateCompany_WithValidInputs_ShouldCreateCompany()
+    {
+        // Arrange
+        string expectedName = "Test Company";
+        string expectedCnpj = "33.113.309/0001-47";
+
+        // Act
+        var company = CompanyDomain.CreateCompany(expectedName, expectedCnpj);
+
+        // Assert
+        Assert.IsNotNull(company);
+        Assert.AreEqual(expectedName, company.Name);
+        Assert.IsNotNull(company.Cnpj);
+        Assert.AreEqual(expectedCnpj, company.Cnpj.ToString());
+    }
+}
