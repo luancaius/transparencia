0ea8d8d5ef7bca889c5969419bcda499713ba18d
luancaius <6966770+luancaius@users.noreply.github.com>
2025-02-22 22:56:59 -0700

    adding monthly party files for 2024

---
+++ src/backend_ruby/crawler/tasks/generate_monthly_parties_files.rb
@@ -0,0 +1,5 @@
+# tasks/generate_monthly_top_deputies_files.rb
+require_relative '../app/services/crawler/monthly_party_cost_file_generator_service'
+
+generator = MonthlyPartyCostFileGeneratorService.new
+generator.generate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 2024)
