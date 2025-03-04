f7e8572fec8ab8fa60dee036b8796ad204b8b8f7
luancaius <6966770+luancaius@users.noreply.github.com>
2025-03-04 15:41:29 -0700

    adding condition to stop getting old data

--- src/backend_ruby/crawler/app/services/crawler/fetch_all_deputies_expenses_service.rb
+++ src/backend_ruby/crawler/app/services/crawler/fetch_all_deputies_expenses_service.rb
@@ -18,8 +18,11 @@ class Crawler::FetchAllDeputiesExpensesService
       puts "Processing deputy #{dep.name} (ID: #{dep.external_id}) for year #{year}..."
 
       # Determine the last month to process: if year is the current year, only iterate until the current month; otherwise, use December.
-      last_month = (year == Time.now.year ? Time.now.month : 12)
-
+      last_month = (year == Time.now.year ?  (Time.now.month - 1) : 12)
+      if last_month < 1
+        puts "  -> No previous month available for the current year. Skipping deputy #{dep.name}."
+        next
+      end
       # 3) Fetch expenses month by month
       (1..last_month).each do |month|
         puts "  Fetching expenses for Month #{month}..."
